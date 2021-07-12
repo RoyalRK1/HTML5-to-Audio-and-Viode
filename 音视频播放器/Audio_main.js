7d225cfabd9c67d24df802422b448e9003e394a1
RoyalRK1 <73464155+RoyalRK1@users.noreply.github.com>
2021-07-12 19:50:27 +0800

    在音乐页面添加唱片图

--- 音视频播放器/Audio_main.js
+++ 音视频播放器/Audio_main.js
@@ -1,11 +1,11 @@
- //添加视频按钮
+ //添加音乐按钮
  $(".add-audio").on('click', function() {
-    //将点击添加视频按钮，定义为点击上传文件的按钮
+    //将点击本地音乐按钮，定义为点击上传文件的按钮
 	$('.audio-file').click();
 });
 
 var audio=[];
-//如果用户选取了自己的视频则通过filereader读取
+//如果用户选取了自己的音乐则通过filereader读取
 $('.audio-file').on('change', function(e) {
     for (var i = 0; i < e.target.files.length; i++) {
         
@@ -22,12 +22,23 @@ $('.audio-file').on('change', function(e) {
     $("audio")[0].src= audio[0].url
     $(".audio-list").html(html);
 });
+//将添加的音乐文件在音乐列表中显示出来
 
-//将添加的视频文件在视频列表中显示出来
 $(".audio-list").on("click", "li", function() {
-    //定义在列表里点击的视频为索引
-    var index = $(".audio-list li").index($(this));
+    //定义在列表里点击的音乐为索引
+	var index = $(".audio-list li").index($(this));
      $("audio")[0].src= audio[index].url
      $("audio")[0].play()
+	//点击列表歌曲时修改指针位置
+	$('#needle').removeClass("pause-needle").addClass("resume-needle")
+
 });
 
+//播放与暂停时唱片上的指针的位置
+function pauseFunction(){
+			$('#needle').removeClass("resume-needle").addClass("pause-needle");
+}
+function playFunction(){
+			$('#needle').removeClass("pause-needle").addClass("resume-needle");
+}
+				
